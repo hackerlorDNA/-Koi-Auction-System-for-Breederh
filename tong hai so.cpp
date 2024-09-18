7f21bd523fdb979e5089b75c300e06dfd2081f50
Du Nhat Anh <nhatanhdu@gmail.com>
2024-09-18 13:39:07 +0700

    tinh tong

---
+++ tong hai so.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, tong;
+    cout << "Nhap so a: ";
+    cin >> a;
+    cout << "Nhap so b: ";
+    cin >> b;
+    tong = a + b;
+    cout << "Tong cua " << a << " va " << b << " la: " << tong << endl;
+    return 0;
+}
